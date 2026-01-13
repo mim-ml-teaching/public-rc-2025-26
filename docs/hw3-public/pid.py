e05585e7612663009a7df3eccf5d88cc4d4f4e88
marekcygan <marek.cygan@gmail.com>
2026-01-13 04:38:03 +0000

    [GHA] Update the test files.

--- docs/hw3-public/pid.py
+++ docs/hw3-public/pid.py
@@ -8,17 +8,11 @@ class PID:
         self.gain_int = gain_int
         self.sensor_period = sensor_period
         # TODO: define additional attributes you might need
-        self.integral = 0
-        self.min_output, self.max_output = output_limits
         # END OF TODO
 
 
     # TODO: implement function which computes the output signal
     # The controller should output only in the range of output_limits
     def output_signal(self, commanded_variable: float, sensor_readings: list[float]) -> float:
-        error = sensor_readings[0] - commanded_variable
-        self.integral += error * self.sensor_period
-        derivative = (sensor_readings[0] - sensor_readings[1]) / self.sensor_period
-        output = self.gain_prop * error + self.gain_der * derivative + self.gain_int * self.integral
-        return max(self.min_output, min(self.max_output, output))
+        return 0
     # END OF TODO
